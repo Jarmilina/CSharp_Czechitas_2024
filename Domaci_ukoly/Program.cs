7c6ecfcb382afdeee1d05d667c078f64edc6cb3a
Jarmilina <128793205+Jarmilina@users.noreply.github.com>
2024-04-11 20:23:46 +0200

    Zkousime git bash.

--- Domaci_ukoly/Program.cs
+++ Domaci_ukoly/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Jupi, C# kurz uz zacina!");
 Console.WriteLine("Jupi, ucime se git!");
+Console.WriteLine("Zkusme git bash!");
